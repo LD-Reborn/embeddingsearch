78f52faf467f6dfde9e163408236ae4e640c085e
LD-Reborn <lucretia.dietz@gmx.de>
2025-12-13 20:58:50 +0100

    Added rudimentary default view

---
+++ src/Server/Controllers/Frontend/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Server.Helper;
+using Shared.Models;
+using Server.Exceptions;
+namespace Server.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController : Controller
+{
+    private readonly ILogger<EntityController> _logger;
+    public HomeController(ILogger<EntityController> logger, IConfiguration config, SearchdomainManager domainManager, SearchdomainHelper searchdomainHelper, DatabaseHelper databaseHelper)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
